8fcbe18da862c67bb9e612e044098e4ea0dafadb
davidontiveros <david.ontiveros.89@hotmail.com>
2013-10-20 23:54:46 -0500

    another problem

--- src/algos/problems/Recursion.java
+++ src/algos/problems/Recursion.java
@@ -1,5 +1,28 @@
 package algos.problems;
 
+import java.util.*;
+
 public class Recursion
 {
+	public static void main(String args[])
+	{
+		TreeSet<Integer> set = new TreeSet<Integer>();	
+		set.add(1);set.add(2);set.add(3);set.add(4);
+		new Recursion().getAllSubsets(set, set.size());
+	}
+	
+	List<Set<Integer>> sets = new ArrayList<Set<Integer>>(); 
+	/**
+	 * Write a method that returns all subsets of a set
+	 */
+	public void getAllSubsets(TreeSet<Integer> set, int offset)
+	{	
+		if(offset < 0)
+			return;				
+		for(int start=1, end=start+offset; end<=set.size();  start++, end++)
+		{			
+			sets.add(set.subSet(start,true, start+offset,true));			
+		}			
+		getAllSubsets(set, --offset);
+	}
 }
