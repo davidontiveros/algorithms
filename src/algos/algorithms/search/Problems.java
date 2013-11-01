dfb4da47da4080f50a7f98f17e5b3d73c63b851d
davidontiveros <david.ontiveros.89@hotmail.com>
2013-11-01 00:12:50 -0600

    another search problem

--- src/algos/algorithms/search/Problems.java
+++ src/algos/algorithms/search/Problems.java
@@ -16,11 +16,26 @@ public class Problems
 	static int moves = 0;
 	public static void main(String[] args) 
 	{
+		/* TEST findElement()*/
+		/*
 		int array[] = {15,16,19,20,25,1,3,4,5,7,10,14};
 		//int array[] = {15,16};
 		int element = 14;
 		int index = new Problems().findElement(array, element);
 		System.out.println(index);
+		System.out.println("moves: "+moves);
+		*/
+		
+		/* TEST findElementInMatrix() */
+		int a[] = {1,2,3,4,5,6};
+		int b[] = {7,8,9,10,11,12};
+		int c[] = {13,14,15,16,17,18};
+		int d[] = {19,20,21,22,23,24};
+		int e[] = {25,26,27,28,29,30};
+		int array[][] = {a,b,c,d,e};
+		int element = 19;
+		
+		System.out.println(new Problems().findElementInMatrix(array, element));
 		System.out.println("moves: "+moves);
 	}
 	
@@ -68,6 +83,36 @@ public class Problems
 		return -1; //not found
 	}
 		
-	
+	/**
+	 * 9.6
+	 * Given a matrix in which each row and each column is sorted, 
+	 * write a method to find an element in it.
+	 * 
+	 * complexity O(log(M+N))
+	 */
+	public String findElementInMatrix(int array[][], int element)
+	{
+		int i = 0;
+		int j = array.length-1;
+		
+		while( i<array.length && j>=0)
+		{
+			if(array[j][i] == element)
+			{
+				return "["+i+"]["+j+"]";
+			}
+			else if(element < array[j][i])
+			{
+				moves++;
+				j--;
+			}
+			else 
+			{
+				moves++;
+				i++;
+			}
+		}
+		return "not found";
+	}
 
 }
